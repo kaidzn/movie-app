2fbcd555cee63658cba0fa05ba0c865374a97fc5
kaidzn <kaidzn17@gmail.com>
2024-02-12 15:17:25 +0600

    Changes to be committed: 	modified:   src/views/HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="feature-card">
       <router-link to="/movie/tt0409591">
-        <img src="https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg" alt="Naruto Poster" class="featured-img" />
+        <img src="https://wallpapers.com/images/featured/naruto-poster-68amfejo0hg34i2t.jpg" alt="Naruto Poster" class="featured-img" />
         <div class="detail">
           <h3>Naruto</h3>
           <p>Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for recognition and dreams of becoming the Hokage, the village's leader and strongest ninja.</p>
