28e86a96c94c628137dc429394154a233952396e
kaidzn <kaidzn17@gmail.com>
2024-02-12 15:09:46 +0600

    On branch main  Changes to be committed: 	modified:   src/App.vue 	deleted:    src/assets/logo.png 	deleted:    src/components/HelloWorld.vue 	new file:   src/env.js 	modified:   src/router/index.js 	deleted:    src/views/AboutView.vue 	modified:   src/views/HomeView.vue 	new file:   src/views/MovieDetail.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <router-link to="/">
+        <h1><span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view/>
+  </main>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family:'Times New Roman', Times, serif;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+&::selection{
+  background-color: transparentize( #42B883, 0.5);
+}
+body{
+  background-color: #35495E;
+}
+a{
+  text-decoration: none;
+}
+header{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3d4e;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.2);
+}
+h1{
+  color: #fff;
+  font-size: 28px;
+  span{
+    color: #42B883;
   }
 }
 </style>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default{
+    apikey: "2650a9a"
+}
